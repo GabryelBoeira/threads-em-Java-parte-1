34dc4eca7872c7625bbe3f826917c704965b80a6
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-08-19 18:58:47 -0300

    Utilizando a Classe Lock
    Utilizando a Classe Lock como alternativa ao synchronized

--- banheiro/src/br/com/alura/banheiro/Banheiro.java
+++ banheiro/src/br/com/alura/banheiro/Banheiro.java
@@ -1,43 +1,55 @@
 package br.com.alura.banheiro;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Banheiro {
+
+    private Lock lock = new ReentrantLock();
 
     public void fazerNumero1() {
         String nome = Thread.currentThread().getName();
         System.out.println(nome + ": batendo na porta");
-        synchronized (this) {
-            System.out.println(nome + ": Entrando no banheiro");
-            System.out.println(nome + ": Fazendo coisa rapida");
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
 
-            System.out.println(nome + ": dando descarga");
-            System.out.println(nome + ": lavando a mão");
-            System.out.println(nome + ": Saindo do banheiro");
+        lock.lock();
+
+        System.out.println(nome + ": Entrando no banheiro");
+        System.out.println(nome + ": Fazendo coisa rapida");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        System.out.println(nome + ": dando descarga");
+        System.out.println(nome + ": lavando a mão");
+        System.out.println(nome + ": Saindo do banheiro");
+
+        lock.unlock();
     }
 
     public void fazerNumero2() {
         String nome = Thread.currentThread().getName();
 
         System.out.println(nome + ": batendo na porta");
-        synchronized (this) {
-            System.out.println(nome + ": Entrando no banheiro");
-            System.out.println(nome + ": Fazendo coisa demorada");
 
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        lock.lock();
 
-            System.out.println(nome + ": dando descarga");
-            System.out.println(nome + ": lavando a mão");
-            System.out.println(nome + ": Saindo do banheiro");
+        System.out.println(nome + ": Entrando no banheiro");
+        System.out.println(nome + ": Fazendo coisa demorada");
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        System.out.println(nome + ": dando descarga");
+        System.out.println(nome + ": lavando a mão");
+        System.out.println(nome + ": Saindo do banheiro");
+
+        lock.unlock();
+
     }
 
 }
