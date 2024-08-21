84afbdf63c96eb1526939fc356935ab8c0e8f842
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-08-21 14:19:41 -0300

    Update Main.java

--- banheiro/src/br/com/alura/banheiro/Main.java
+++ banheiro/src/br/com/alura/banheiro/Main.java
@@ -8,8 +8,6 @@ public class Main {
 
         Thread convidado1 = new Thread(new TarefaNumero1(banheiro), "Convidado 1");
         Thread convidado2 = new Thread(new TarefaNumero2(banheiro), "Convidado 2");
-//        Thread convidado3 = new Thread(new TarefaNumero1(banheiro), "Convidado 3");
-//        Thread convidado4 = new Thread(new TarefaNumero2(banheiro), "Convidado 4");
 
         Thread limpeza = new Thread(new TarefaNumero3(banheiro), "Limpeza 1");
         limpeza.setDaemon(true);
@@ -19,8 +17,6 @@ public class Main {
         limpeza.start();
         convidado1.start();
         convidado2.start();
-//        convidado3.start();
-//        convidado4.start();
     }
 
 }
